c701999625be893e350b00e64c8393db52e308fe
Alexandre de Oliveira <alexmontanha@hotmail.com>
2022-10-07 20:17:53 -0300

    Atividade 1 da lista 5

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Entre com o número de linhas: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Sua matriz ficará assim: ");
+
+        imprimirLinhas(n);
+
+        sc.close();
+    }
+
+    public static void imprimirLinhas(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "  ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
